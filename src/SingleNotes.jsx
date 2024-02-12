38a4ceb93dd0000e6097c1a4e57c2b2f84a58d24
love-sonkar <loveboss806@gmail.com>
2024-02-12 22:38:35 +0530

    added update functionality

--- src/SingleNotes.jsx
+++ src/SingleNotes.jsx
@@ -1,14 +1,28 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const SingleNotes = () => {
   const [singleNote, setSingleNotes] = useState();
+  const [edit, setEdit] = useState(false);
+  const navigate = useNavigate();
 
   const param = useParams();
+  const id = param.id;
   const fetchSingleNote = async () => {
-    const res = await axios.get(`http://localhost:8080/notes/${param.id}`);
+    const res = await axios.get(`http://localhost:8080/notes/${id}`);
     setSingleNotes(res.data);
+  };
+
+  const editNote = async () => {
+    const res = await axios.put(
+      `http://localhost:8080/notes/${id}`,
+      singleNote
+    );
+    if (res.status === 200) {
+      alert("Note updated successfully");
+    }
+    navigate("/");
   };
 
   useEffect(() => {
@@ -19,11 +33,28 @@ const SingleNotes = () => {
     <div className="text-center p-4">
       <h2>Your Note</h2>
       {singleNote ? (
-        <h2 className="bg-blue-400 w-full p-4 text-white rounded-lg">
-          {singleNote.notesItem}
-        </h2>
+        <>
+          {edit ? (
+            <input
+              type="text"
+              value={singleNote.notesItem}
+              onChange={(e) =>
+                setSingleNotes({ ...singleNote, notesItem: e.target.value })
+              }
+            />
+          ) : (
+            <h2 className="bg-blue-400 w-full p-4 text-white rounded-lg">
+              {singleNote.notesItem}
+            </h2>
+          )}
+        </>
       ) : (
-        "nothing"
+        "Loading..."
+      )}
+      {!edit ? (
+        <button onClick={() => setEdit(true)}>Edit</button>
+      ) : (
+        <button onClick={editNote}>Save</button>
       )}
     </div>
   );
